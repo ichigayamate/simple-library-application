e08e2b0f9ffe134dbea0d5c6db6840b64f078cfa
ichigayamate <kyokodenshae353kei@gmail.com>
2025-04-21 16:44:47 +0700

    added initial routes for server

--- server/index.js
+++ server/index.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const cors = require("cors");
 
+require("dotenv").config();
+const mongoose = require("mongoose");
+
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+  console.log("Connected to MongoDB");
+}).catch((err) => {
+  console.error("Error connecting to MongoDB:", err);
+});
+
+app.use("/api", require("./routes/"));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
---
+++ server/routes/index.js
@@ -0,0 +1,5 @@
+const router = require('express').Router();
+
+// add route
+
+module.exports = router;
